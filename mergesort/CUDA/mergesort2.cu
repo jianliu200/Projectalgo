14b0cec9bd7e9e421459aa5a3841dfe7c5eaa741
jianliu200 <94575804+jianliu200@users.noreply.github.com>
2023-11-09 23:28:20 -0600

    added the CUDA cali

--- mergesort/CUDA/mergesort2.cu
+++ mergesort/CUDA/mergesort2.cu
@@ -1,128 +1,212 @@
-
-/**
- * mergesort.cu
- * a one-file c++ / cuda program for performing mergesort on the GPU
- * While the program execution is fairly slow, most of its runnning time
- *  is spent allocating memory on the GPU.
- * For a more complex program that performs many calculations,
- *  running on the GPU may provide a significant boost in performance
- */
-
-// helper for main()
-
-// source: https://github.com/kevin-albert/cuda-mergesort/blob/master/mergesort.cu
-// author: kevin-albert
-// I am using this source code for CUDA implementation of merge sort. I have added caliper and adiak annotations to the code.
-
-
-#include <iostream>
-//#include <helper_cuda.h>
-#include <sys/time.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 #include <caliper/cali.h>
 #include <caliper/cali-manager.h>
 #include <adiak.hpp>
-#include <algorithm>
-//#include <helper_cuda.h>
 
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
+#define OK 1
+#define EXPECTATION_ERROR 1
+#define MALLOC_ERROR 2
+#define CUDA_ERROR 3
 
-//long readList(long**);
+// source: https://github.com/chrishadi/cuda-sort/tree/main
+// author: Chris Hadi
+// I am using this source code for CUDA implementation of merge sort. I have added caliper and adiak annotations to the code.
 
-// data[], size, threads, blocks, 
-void mergesort(long*, long, dim3, dim3);
-// A[]. B[], size, width, slices, nThreads
-__global__ void gpu_mergesort(long*, long*, long, long, long, dim3*, dim3*);
-__device__ void gpu_bottomUpMerge(long*, long*, long, long, long);
-
-// const char* main_loop = "main loop";
-// const char* comm =  "comm";
-// const char* comm_large = "comm_large";
-// const char* comp = "comp";
-// const char* barrier = "barrier";
-// const char* scatter = "scatter";
-// const char* gather = "gather";
-// const char* data_init = "data_init";
-// const char* correctness = "correctness";
-
-// profiling
-//int tm();
-
-#define min(a, b) (a < b ? a : b)
 const char* main_loop = "main loop";
 const char* comm =  "comm";
 const char* comm_large = "comm_large";
+const char* small_comm1 = "small_comm1";
+const char* small_comm2 = "small_comm2";
 const char* comp = "comp";
 const char* comp_large = "comp_large";
 const char* data_init = "data_init";
 const char* correctness = "correctness";
 
-bool verbose = true;
-int main(int argc, char** argv) {
-    printf("I have reached here\n");
+
+cudaError_t mergeSortWithCuda(int* arr, unsigned int count);
+bool assertArrEq(int* expected, int* actual, size_t size);
+int testMergeSortWithCuda(int* actual, int* expected, const unsigned int count);
+
+typedef struct mergeSortResult {
+    cudaError_t cudaStatus;
+    char* msg;
+} mergeSortResult_t;
+
+__global__ void mergeSortKernel(int* arr, int* aux, unsigned int blockSize, const unsigned int last)
+{
+    int x = threadIdx.x;
+    int start = blockSize * x;
+    int end = start + blockSize - 1;
+    int mid = start + (blockSize / 2) - 1;
+    int l = start, r = mid + 1, i = start;
+
+    if (end > last) { end = last; }
+    if (start == end || end <= mid) { return; }
+
+    while (l <= mid && r <= end) {
+        if (arr[l] <= arr[r]) {
+            aux[i++] = arr[l++];
+        }
+        else {
+            aux[i++] = arr[r++];
+        }
+    }
+
+    while (l <= mid) { aux[i++] = arr[l++]; }
+    while (r <= end) { aux[i++] = arr[r++]; }
+
+    for (i = start; i <= end; i++) {
+        arr[i] = aux[i];
+    }
+}
+
+inline mergeSortResult_t mergeSortError(cudaError_t cudaStatus, char* msg) {
+    mergeSortResult_t error;
+    error.cudaStatus = cudaStatus;
+    error.msg = msg;
+    return error;
+}
+
+inline mergeSortResult_t mergeSortSuccess() {
+    mergeSortResult_t success;
+    success.cudaStatus = cudaSuccess;
+    return success;
+}
+
+inline mergeSortResult_t doMergeSortWithCuda(int* arr, unsigned int count, int* dev_arr, int* dev_aux) {
+    const unsigned int last = count - 1;
+    const unsigned size = count * sizeof(int);
+    unsigned int threadCount;
+    cudaError_t cudaStatus;
+    char msg[1024];
+
+    // Copy input vectors from host memory to GPU buffers.
+    CALI_MARK_BEGIN(comm);
+    CALI_MARK_BEGIN(comm_large);
+
+    CALI_MARK_BEGIN(small_comm1);
+    cudaStatus = cudaMemcpy(dev_arr, arr, size, cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        return mergeSortError(cudaStatus, "cudaMemcpy failed!");
+    }
+    CALI_MARK_END(small_comm1);
+
+    CALI_MARK_BEGIN(comp);
+    CALI_MARK_BEGIN(comp_large);
+    for (unsigned int blockSize = 2; blockSize < 2 * count; blockSize *= 2) {
+        threadCount = count / blockSize;
+        if (count % blockSize > 0) { threadCount++; }
+
+        // Launch a kernel on the GPU with one thread for each block.
+        mergeSortKernel<<<1, threadCount>>>(dev_arr, dev_aux, blockSize, last);
+
+        // Check for any errors launching the kernel
+        cudaStatus = cudaGetLastError();
+        if (cudaStatus != cudaSuccess) {
+            sprintf(msg, "mergeSortKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+            return mergeSortError(cudaStatus, msg);
+        }
+
+        // cudaDeviceSynchronize waits for the kernel to finish, and returns
+        // any errors encountered during the launch.
+        cudaStatus = cudaDeviceSynchronize();
+        if (cudaStatus != cudaSuccess) {
+            sprintf(msg, "cudaDeviceSynchronize returned error code %d after launching mergeSortKernel!\n", cudaStatus);
+            return mergeSortError(cudaStatus, msg);
+        }
+    }
+    CALI_MARK_END(comp_large);
+    CALI_MARK_END(comp);
+    // Copy output vector from GPU buffer to host memory.
+    CALI_MARK_BEGIN(small_comm2);
+    cudaStatus = cudaMemcpy(arr, dev_arr, size, cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        return mergeSortError(cudaStatus, "cudaMemcpy failed!");
+    }
+    CALI_MARK_END(small_comm2);
+    CALI_MARK_END(comm_large);
+    CALI_MARK_END(comm);
+
+    return mergeSortSuccess();
+}
+
+cudaError_t mergeSortWithCuda(int* arr, unsigned int count)
+{
+    const unsigned int size = count * sizeof(int);
+    int* dev_arr = 0;
+    int* dev_aux = 0;
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed! Do you have a CUDA-capable GPU installed?");
+        return cudaStatus;
+    }
+
+    // Allocate GPU buffers for two vectors (main and aux array).
+    cudaStatus = cudaMalloc((void**)&dev_arr, size);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        return cudaStatus;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_aux, size);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        cudaFree(dev_arr);
+        return cudaStatus;
+    }
+
+    mergeSortResult_t result = doMergeSortWithCuda(arr, count, dev_arr, dev_aux);
+
+    if (result.cudaStatus != cudaSuccess) {
+        fprintf(stderr, result.msg);
+    }
+
+    cudaFree(dev_arr);
+    cudaFree(dev_aux);
+
+    return cudaStatus;
+}
+
+int main()
+{
     CALI_CXX_MARK_FUNCTION;
     cali::ConfigManager mgr;
 	mgr.start();
     CALI_MARK_BEGIN(main_loop);
-    long* data;
-    long size = atoi(argv[2]);
-    CALI_MARK_BEGIN(data_init);
-    for(int i = 0; i < size; i++)
-    {
-        data[i] = rand() % size;
+    const unsigned int count = 64;
+    const unsigned int size = count * sizeof(int);
+    int status = MALLOC_ERROR;
+    int* actual = (int*) malloc(size);
+    int* expected = (int*) malloc(size);
+
+    if (actual != NULL && expected != NULL) {
+        status = testMergeSortWithCuda(actual, expected, count);
     }
-    CALI_MARK_END(data_init);
-    if (!size) return -1;
-    dim3 threadsPerBlock;
-    dim3 blocksPerGrid;
-
-    threadsPerBlock.x = 32;// atoi(argv[1]);
-    threadsPerBlock.y = 1;
-    threadsPerBlock.z = 1;
-
-    blocksPerGrid.x = 8;//atoi(argv[1])/size;
-    blocksPerGrid.y = 1;
-    blocksPerGrid.z = 1;
-
-    // if (verbose)
-    //     std::cout << "sorting " << size << " numbers\n\n";
-
-    // merge-sort the data
-    
-    
-    mergesort(data, size, threadsPerBlock, blocksPerGrid);
-    
-    
-    //tm();
-
-    //
-    // Print out the list
-    //
-    for (int i = 0; i < size; i++) {
-        std::cout << data[i] << '\n';
-    } 
-    
-    CALI_MARK_BEGIN(correctness);
-    bool sorting = std::is_sorted(data, data + size);
-    CALI_MARK_END(correctness);
-    if(sorting == true)
-    {
-        std::cout << "The array is sorted\n";
-    }
-    else
-    {
-        std::cout << "The array is not sorted\n";
+    else {
+        fprintf(stderr, "malloc failed!");
     }
 
-    // if (verbose) {
-    //     std::cout << "print list to stdout: " << tm() << " microseconds\n";
-    // }
-    printf("I have reached here2\n");
+    free(actual);
+    free(expected);
 
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    int cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return CUDA_ERROR;
+    }
     CALI_MARK_END(main_loop);
     mgr.stop();
    	mgr.flush();
-
     adiak::init(NULL);
     adiak::launchdate();    // launch date of the job
     adiak::libraries();     // Libraries used
@@ -130,214 +214,50 @@ int main(int argc, char** argv) {
     adiak::clustername();   // Name of the cluster
     adiak::value("Algorithm", "Merge sort"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
     adiak::value("ProgrammingModel", "CUDA"); // e.g., "MPI", "CUDA", "MPIwithCUDA"
-    adiak::value("Datatype", "long"); // The datatype of input elements (e.g., double, int, float)
+    adiak::value("Datatype", "int"); // The datatype of input elements (e.g., double, int, float)
     adiak::value("SizeOfDatatype", "4 bytes"); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
-    adiak::value("InputSize", size); // The number of elements in input dataset (1000)
+    adiak::value("InputSize", count); // The number of elements in input dataset (1000)
     adiak::value("InputType", "Random"); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
     //adiak::value("num_procs", world_size); // The number of processors (MPI ranks)
-    adiak::value("num_threads", threadsPerBlock.x); // The number of CUDA or OpenMP threads
-    adiak::value("num_blocks", blocksPerGrid.x); // The number of CUDA blocks 
+    adiak::value("num_threads", count); // The number of CUDA or OpenMP threads
+    adiak::value("num_blocks", count); // The number of CUDA blocks 
     adiak::value("group_num", "7"); // The number of your group (integer, e.g., 1, 10)
-    adiak::value("implementation_source", "Online: https://github.com/kevin-albert/cuda-mergesort/blob/master/mergesort.cu"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
+    adiak::value("implementation_source", "Online: https://github.com/chrishadi/cuda-sort/tree/main"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
 
+    return status;
 }
 
-void mergesort(long* data, long size, dim3 threadsPerBlock, dim3 blocksPerGrid) {
+int cmpInt(const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
 
-    //
-    // Allocate two arrays on the GPU
-    // we switch back and forth between them during the sort
-    //
-    long* D_data;
-    long* D_swp;
-    dim3* D_threads;
-    dim3* D_blocks;
-    
-    // Actually allocate the two arrays
-    CALI_MARK_BEGIN(comm);
-    CALI_MARK_BEGIN(comm_large);
-    //tm();
-    cudaMalloc((void**) &D_data, size * sizeof(long));
-    cudaMalloc((void**) &D_swp, size * sizeof(long));
-    // if (verbose)
-    //     std::cout << "cudaMalloc device lists: " << tm() << " microseconds\n";
-
-    // Copy from our input list into the first array
-    cudaMemcpy(D_data, data, size * sizeof(long), cudaMemcpyHostToDevice);
-    // if (verbose) 
-    //     std::cout << "cudaMemcpy list to device: " << tm() << " microseconds\n";
- 
-    //
-    // Copy the thread / block info to the GPU as well
-    //
-    cudaMalloc((void**) &D_threads, sizeof(dim3));
-    cudaMalloc((void**) &D_blocks, sizeof(dim3));
-
-    // if (verbose)
-    //     std::cout << "cudaMalloc device thread data: " << tm() << " microseconds\n";
-    cudaMemcpy(D_threads, &threadsPerBlock, sizeof(dim3), cudaMemcpyHostToDevice);
-    cudaMemcpy(D_blocks, &blocksPerGrid, sizeof(dim3), cudaMemcpyHostToDevice);
-
-    CALI_MARK_END(comm_large);
-    CALI_MARK_END(comm);
-    
-    // if (verbose)
-    //     std::cout << "cudaMemcpy thread data to device: " << tm() << " microseconds\n";
-
-    long* A = D_data;
-    long* B = D_swp;
-
-    long nThreads = threadsPerBlock.x * threadsPerBlock.y * threadsPerBlock.z *
-                    blocksPerGrid.x * blocksPerGrid.y * blocksPerGrid.z;
-
-    //
-    // Slice up the list and give pieces of it to each thread, letting the pieces grow
-    // bigger and bigger until the whole list is sorted
-    //
-    CALI_MARK_BEGIN(comp);
-    CALI_MARK_BEGIN(comp_large);
-    printf("Before the comp\n");
-    for (int width = 2; width < (size << 1); width <<= 1) {
-        long slices = size / ((nThreads) * width) + 1;
-
-        // if (verbose) {
-        //     std::cout << "mergeSort - width: " << width 
-        //               << ", slices: " << slices 
-        //               << ", nThreads: " << nThreads << '\n';
-        //     tm();
-        // }
-
-        // Actually call the kernel
-        gpu_mergesort<<<blocksPerGrid, threadsPerBlock>>>(A, B, size, width, slices, D_threads, D_blocks);
-
-        // if (verbose)
-        //     std::cout << "call mergesort kernel: " << tm() << " microseconds\n";
-
-        // Switch the input / output arrays instead of copying them around
-        A = A == D_data ? D_swp : D_data;
-        B = B == D_data ? D_swp : D_data;
+int testMergeSortWithCuda(int* actual, int* expected, const unsigned int count) {
+    CALI_MARK_BEGIN(data_init);
+    for (unsigned int i = 0; i < count; i++) {
+        expected[i] = actual[i] = rand();
     }
-    CALI_MARK_END(comp_large);
-    CALI_MARK_END(comp);
-    printf("After the comp\n");
-    //
-    // Get the list back from the GPU
-    //
-    //tm();
-    cudaMemcpy(data, A, size * sizeof(long), cudaMemcpyDeviceToHost);
-    // if (verbose)
-    //     std::cout << "cudaMemcpy list back to host: " << tm() << " microseconds\n";
-    
-    
-    // Free the GPU memory
-    cudaFree(A);
-    cudaFree(B);
-    // if (verbose)
-    //     std::cout << "cudaFree: " << tm() << " microseconds\n";
-}
+    CALI_MARK_END(data_init);
 
-// GPU helper function
-// calculate the id of the current thread
-__device__ unsigned int getIdx(dim3* threads, dim3* blocks) {
-    int x;
-    return threadIdx.x +
-           threadIdx.y * (x  = threads->x) +
-           threadIdx.z * (x *= threads->y) +
-           blockIdx.x  * (x *= threads->z) +
-           blockIdx.y  * (x *= blocks->z) +
-           blockIdx.z  * (x *= blocks->y);
-}
+    qsort(expected, count, sizeof(int), cmpInt);
 
-//
-// Perform a full mergesort on our section of the data.
-//
-__global__ void gpu_mergesort(long* source, long* dest, long size, long width, long slices, dim3* threads, dim3* blocks) {
-    unsigned int idx = getIdx(threads, blocks);
-    long start = width*idx*slices, 
-         middle, 
-         end;
-
-    for (long slice = 0; slice < slices; slice++) {
-        if (start >= size)
-            break;
-
-        middle = min(start + (width >> 1), size);
-        end = min(start + width, size);
-        gpu_bottomUpMerge(source, dest, start, middle, end);
-        start += width;
+    cudaError_t cudaStatus = mergeSortWithCuda(actual, count);
+    CALI_MARK_BEGIN(correctness);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "mergeSortWithCuda failed!");
+        return CUDA_ERROR;
     }
-}
 
-//
-// Finally, sort something
-// gets called by gpu_mergesort() for each slice
-//
-__device__ void gpu_bottomUpMerge(long* source, long* dest, long start, long middle, long end) {
-    long i = start;
-    long j = middle;
-    for (long k = start; k < end; k++) {
-        if (i < middle && (j >= end || source[i] < source[j])) {
-            dest[k] = source[i];
-            i++;
-        } else {
-            dest[k] = source[j];
-            j++;
-        }
+    if (!assertArrEq(expected, actual, count * sizeof(int))) {
+        puts("cuda sorted array is not equal to the qsorted array!");
+        return EXPECTATION_ERROR;
     }
+    puts("This is sorted by mergesort correctly");
+    puts("test ok.");
+    CALI_MARK_END(correctness);
+    return OK;
+    
 }
 
-// read data into a minimal linked list
-typedef struct {
-    int v;
-    void* next;
-} LinkNode;
-
-// helper function for reading numbers from stdin
-// it's 'optimized' not to check validity of the characters it reads in..
-// long readList(long** list) {
-//     tm();
-//     long v, size = 0;
-//     LinkNode* node = 0;
-//     LinkNode* first = 0;
-//     while (std::cin >> v) {
-//         LinkNode* next = new LinkNode();
-//         next->v = v;
-//         if (node)
-//             node->next = next;
-//         else 
-//             first = next;
-//         node = next;
-//         size++;
-//     }
-
-
-//     if (size) {
-//         *list = new long[size]; 
-//         LinkNode* node = first;
-//         long i = 0;
-//         while (node) {
-//             (*list)[i++] = node->v;
-//             node = (LinkNode*) node->next;
-//         }
-
-//     }
-
-//     // if (verbose)
-//     //     std::cout << "read stdin: " << tm() << " microseconds\n";
-
-//     return size;
-// }
-
-
-// 
-// Get the time (in microseconds) since the last call to tm();
-// the first value returned by this must not be trusted
-//
-// timeval tStart;
-// int tm() {
-//     timeval tEnd;
-//     gettimeofday(&tEnd, 0);
-//     int t = (tEnd.tv_sec - tStart.tv_sec) * 1000000 + tEnd.tv_usec - tStart.tv_usec;
-//     tStart = tEnd;
-//     return t;
-// }
+bool assertArrEq(int* expected, int* actual, size_t size) {
+    return memcmp(expected, actual, size) == 0;
+}
